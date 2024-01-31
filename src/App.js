c2bc07178328da9aa9121370e4f839f111f4a277
lfouuu <boukharifourat61@gmail.com>
2024-01-31 16:40:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import ListUsers from './Components/ListUsers';
+import NavUser from './Components/NavUser';
+import Home from './Components/Home';
+import Profil from './Components/Profil';
+import { useState } from 'react';
+import PrivateRoute from './Components/PrivateRoute';
+
+function App() {
+  const [auth,setAuth] = useState(false)
+  return (
+    <div>
+      <NavUser auth={auth} setAuth={setAuth}/>
+
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/ListUsers' element={<PrivateRoute auth={auth}><ListUsers/></PrivateRoute>}/>
+          <Route path='/Profil/:id' element={<PrivateRoute auth={auth}><Profil/></PrivateRoute>}/>
+      </Routes>
+   
+    </div>
+  );
+}
+
+export default App;
